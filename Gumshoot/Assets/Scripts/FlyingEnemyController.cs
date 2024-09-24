5a1beacb9e866c461f6c8120e0037eec72bf62f4
YtG12 <151299641+YtG12@users.noreply.github.com>
2024-09-24 15:12:30 -0700

    Add Cooldown to FlyingPlayer

--- Gumshoot/Assets/Scripts/FlyingEnemyController.cs
+++ Gumshoot/Assets/Scripts/FlyingEnemyController.cs
@@ -6,13 +6,33 @@ public class FlyingEnemyController : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
+    public GameObject playerPrefab;
+    private bool isCooldown = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CooldownCoroutine());
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        if (!isCooldown)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0f);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0f);
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    IEnumerator CooldownCoroutine()
+    {
+        yield return new WaitForSeconds(5f);
+        isCooldown = true;
+        Instantiate(playerPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 }
